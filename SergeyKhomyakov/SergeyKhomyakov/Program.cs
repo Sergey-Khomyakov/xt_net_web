9b6b4c4bca59f6563087e6c27fdd021b99eed0e7
Сергей Хомяков <ser.xomiakoff@yandex.ru>
2019-11-14 13:31:47 +0400

    Add method Sequence

---
+++ SergeyKhomyakov/SergeyKhomyakov/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task0_Intro
+{
+    class Program
+    {
+        static void Sequence(int getNumber) 
+        {
+            for (int i = 1; i < getNumber + 1; i++) 
+            {
+                if (i == getNumber)
+                {
+                    Console.Write(i);
+                }
+                else 
+                {
+                    Console.Write($"{i},");
+                }               
+            }
+        }
+        static void Main(string[] args)
+        {
+            string getNumber = string.Empty;
+            int number;
+
+            Console.Write("Введите положительное число: ");
+            getNumber = Console.ReadLine();
+
+            while (!int.TryParse(getNumber, out number) || number < 0)
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine("Вы ввели отрицательное число");
+                }
+                else 
+                {
+                    Console.WriteLine("Я не понимаю что вы ввели :(((");               
+                }
+                Console.WriteLine();
+
+                Console.Write("Введите положительное число: ");
+                getNumber = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            Sequence(number);
+
+            Console.ReadKey();
+        }
+    }
+}
