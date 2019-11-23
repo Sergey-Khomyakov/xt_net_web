ea98ea94940df7a178c273a5ab21d85ea5a84f61
Сергей Хомяков <ser.xomiakoff@yandex.ru>
2019-11-23 14:33:50 +0400

    Add Task 2.1

---
+++ SergeyKhomyakov/ExperimentalTraining.Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ExperimentalTraining.Task2
+{
+    class Program
+    {
+        private static int СheckInputNumber()
+        {
+            var stringNumber = string.Empty;
+            int number;
+
+            stringNumber = Console.ReadLine();
+
+            while (!int.TryParse(stringNumber, out number))
+            {
+                Console.WriteLine("Я не понимаю что вы ввели :(((");
+                Console.WriteLine();
+
+                Console.Write("Введите число: ");
+                stringNumber = Console.ReadLine();
+                Console.WriteLine();
+            }
+            return number;
+        }
+        private static void ShowRound() 
+        {
+            Console.Write("Введите координату Х - ");
+            int x = СheckInputNumber();
+            Console.Write("Введите координату Y - ");
+            int y = СheckInputNumber();
+            Console.Write("Введите радиус R - ");
+            int r = СheckInputNumber();
+            Round round = new Round(x,y,r);
+            round.ShowRound();
+        }
+        static void Main(string[] args)
+        {
+            string[] menuTask = new string[] { "1 - ROUND", "2 - TRIANGLE", "3 - USER", "4 - MY STRING", "5 - EMPLOYEE", "6 - RING", "7 - VECTOR GRAPHICS EDITOR", "8 - GAME", "0 - EXIT"};
+            bool isExit = false;
+            int numberTask;
+            while (!isExit) 
+            {
+                Console.WriteLine("********************************");
+                foreach (var task in menuTask)
+                {
+                    Console.WriteLine(task);
+                }
+                Console.WriteLine("********************************");
+                Console.Write("Задание № ");
+                numberTask = СheckInputNumber();
+                switch (numberTask) 
+                {
+                    case 0:
+                        isExit = true;
+                        break;
+                    case 1:
+                        ShowRound();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SergeyKhomyakov/ExperimentalTraining.Task2/Round.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExperimentalTraining.Task2
+{
+
+    class Round
+    {
+        private double R { get; set; }
+        private double X { get; set; }
+        private double Y { get; set; }
+        public Round(double x, double y, double r)
+        {
+            this.X = x;
+            this.Y = y;
+            if (r > 0)
+            {
+                this.R = r;
+            }
+            else
+            {
+                throw new ArgumentException(" Wrong !!! The radius can not be negative.");
+            }
+            
+            
+        }
+        private double Square
+        {
+            get
+            {
+                return Math.PI * R * R;
+            }
+        }
+        private double Length 
+        {
+            get
+            {
+                return 2 * Math.PI * R;
+            }
+        }
+        public void ShowRound()
+        {
+            Console.WriteLine("X = {0} Y = {1} R = {2}", X, Y, R);
+            Console.WriteLine("Длина круга = {0:0.##}", Length);
+            Console.WriteLine("Площадь круга = {0:0.##}", Square);
+        }
+    }
+}
